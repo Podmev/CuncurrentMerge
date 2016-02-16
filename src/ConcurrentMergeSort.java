2075f8a37266f8c049398195231e79f9349c63e9
podmev <podmev@gmail.com>
2016-02-16 21:56:03 +0300

    Init commit

---
+++ src/ConcurrentMergeSort.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+/**
+ * Created by Дмитрий on 16.02.2016.
+ */
+public class ConcurrentMergeSort<T> implements Sortable<T> {
+    @Override
+    public List<T> sort(List<T> list) {
+        return null;
+    }
+}
